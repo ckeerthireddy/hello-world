7f37e7daa96caf9df084271bd2842bc555db1d41
ckeerthireddy <31797100+ckeerthireddy@users.noreply.github.com>
2017-09-11 13:50:39 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,12 @@
+public class Main {
+
+	public static void main(String[] args) {
+		Main st=new Main();
+
+		st.setStudents();
+		//You may test that your code works find here
+		//Please check that your code works and has no
+		//compilation problems before to submit
+	}
+
+}
